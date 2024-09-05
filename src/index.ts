8f0578d3caf6a28cda3afa7e77e5fe1c0b4ae5d9
rafaeldoering <rafael.doering.soares@usp.br>
2024-09-04 22:56:59 -0300

    Add commander

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .version("0.0.1")
+  .description("Environment Runner")
+  .option('--first')
+  .option('-s, --separator <char>');
+
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
